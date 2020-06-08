ffaed33d1de6c391bb1da2182e1fdb9f7b5bf140
Tanay Mukherjee <its_tanay_here@yahoo.com>
2020-06-07 20:00:14 -0400

    Update Date manipulation.sql

--- Date manipulation.sql
+++ Date manipulation.sql
@@ -1,1 +1,49 @@
+-- Write a query to determine how many transactions exist per day.
+-- Use of CONVERT function:
+SELECT
+  -- Select the date portion of StartDate
+  CONVERT(DATE, StartDate) as StartDate,
+  -- Measure how many records exist for each StartDate
+  COUNT(ID) as CountOfRows 
+FROM CapitalBikeShare 
+-- Group by the date portion of StartDate
+GROUP BY CONVERT(DATE, StartDate)
+-- Sort the results by the date portion of StartDate
+ORDER BY CONVERT(DATE, StartDate);
+-- StartDate format in the table is as 2018-03-01 00:02:41
 
+
+
+-- Complete the first CASE statement, using DATEPART() to evaluate the SECOND date part of StartDate.
+-- Use of DATEPART function:
+SELECT
+	-- Count the number of IDs
+	COUNT(ID) AS Count,
+    -- Use DATEPART() to evaluate the SECOND part of StartDate
+    "StartDate" = CASE WHEN DATEPART(SECOND, StartDate) = 0 THEN 'SECONDS = 0'
+					   WHEN DATEPART(SECOND, StartDate) > 0 THEN 'SECONDS > 0' END
+FROM CapitalBikeShare
+GROUP BY
+    -- Complete the CASE statement
+	CASE WHEN DATEPART(SECOND, StartDate) = 0 THEN 'SECONDS = 0'
+		 WHEN DATEPART(SECOND, StartDate) > 0 THEN 'SECONDS > 0' END
+
+
+
+
+-- Which day of week is busiest?
+-- Use of DATENAME, DATEDIFF functions:
+SELECT
+    -- Select the day of week value for StartDate
+	DATENAME(weekday, StartDate) as DayOfWeek,
+    -- Calculate TotalTripHours
+	SUM(DATEDIFF(second, StartDate, EndDate))/ 3600 as TotalTripHours 
+FROM CapitalBikeShare 
+-- Group by the day of week
+GROUP BY DATENAME(weekday, StartDate)
+-- Order TotalTripHours in descending order
+ORDER BY TotalTripHours DESC
+               
+               
+               
+-- 
