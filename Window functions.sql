33f242bba7c6a91795bb534e3d3eea2674955707
Tanay Mukherjee <its_tanay_here@yahoo.com>
2020-06-09 03:55:05 -0400

    Update Window functions.sql

--- Window functions.sql
+++ Window functions.sql
@@ -175,7 +175,7 @@ FROM Orders
 
 
 
--- Calculating mode
+-- Calculating mode:
 -- Example 1:
 -- Create a CTE ModePrice that returns two columns (OrderPrice and UnitPriceFrequency)
 -- Create a CTE Called ModePrice which contains two columns
@@ -204,8 +204,227 @@ AS
 	FROM Orders
 )
 
--- Select the order price from the CTE
+-- Select the order price from the CTE:
 SELECT OrderPrice AS ModeOrderPrice
 FROM ModePrice
 -- Select the maximum UnitPriceFrequency from the CTE
 WHERE UnitPriceFrequency IN (SELECT MAX(UnitPriceFrequency) From ModePrice)
+
+	      
+
+-- Working with statistical aggregate functions:
+SELECT
+	it.IncidentType,
+	AVG(ir.NumberOfIncidents) AS MeanNumberOfIncidents,
+	AVG(CAST(ir.NumberOfIncidents AS DECIMAL(4,2))) AS MeanNumberOfIncidents,
+	STDEV(ir.NumberOfIncidents) AS NumberOfIncidentsStandardDeviation,
+	VAR(ir.NumberOfIncidents) AS NumberOfIncidentsVariance,
+	COUNT(1) AS NumberOfRows
+FROM dbo.IncidentRollup ir
+	INNER JOIN dbo.IncidentType it
+		ON ir.IncidentTypeID = it.IncidentTypeID
+	INNER JOIN dbo.Calendar c
+		ON ir.IncidentDate = c.Date
+WHERE
+	c.CalendarQuarter = 2
+	AND c.CalendarYear = 2020
+GROUP BY
+it.IncidentType;
+	      
+	      
+	      	      
+-- Calculating median in SQL Server:
+-- There is no MEDIAN() function in SQL Server. The closest we have is PERCENTILE_CONT(), which finds the value at the nth percentile across a data set.
+SELECT DISTINCT
+	it.IncidentType,
+	AVG(CAST(ir.NumberOfIncidents AS DECIMAL(4,2)))
+	    OVER(PARTITION BY it.IncidentType) AS MeanNumberOfIncidents,
+	PERCENTILE_CONT(0.5)
+    	-- Inside our group, order by number of incidents DESC
+    	WITHIN GROUP (ORDER BY ir.NumberOfIncidents DESC)
+        -- Do this for each IncidentType value
+        OVER (PARTITION BY it.IncidentType) AS MedianNumberOfIncidents,
+	COUNT(1) OVER (PARTITION BY it.IncidentType) AS NumberOfRows
+FROM dbo.IncidentRollup ir
+	INNER JOIN dbo.IncidentType it
+		ON ir.IncidentTypeID = it.IncidentTypeID
+	INNER JOIN dbo.Calendar c
+		ON ir.IncidentDate = c.Date
+WHERE
+	c.CalendarQuarter = 2
+	AND c.CalendarYear = 2020;
+						 
+						 
+						 
+-- Downsample to a daily grain:
+-- Example 1: (Daily grain)
+SELECT
+	-- Downsample to a daily grain
+    -- Cast CustomerVisitStart as a date
+	CAST(dsv.CustomerVisitStart AS DATE) AS Day,
+	SUM(dsv.AmenityUseInMinutes) AS AmenityUseInMinutes,
+	COUNT(1) AS NumberOfAttendees
+FROM dbo.DaySpaVisit dsv
+WHERE
+	dsv.CustomerVisitStart >= '2020-06-11'
+	AND dsv.CustomerVisitStart < '2020-06-23'
+GROUP BY
+	CAST(dsv.CustomerVisitStart AS DATE)
+ORDER BY
+	Day;
+						 
+-- Example 2: (Weekly grain)
+SELECT
+	-- Downsample to a weekly grain
+	DATEPART(WEEK, dsv.CustomerVisitStart) AS Week,
+	SUM(dsv.AmenityUseInMinutes) AS AmenityUseInMinutes,
+	-- Find the customer with the largest customer ID for that week
+	MAX(dsv.CustomerID) AS HighestCustomerID,
+	COUNT(1) AS NumberOfAttendees
+FROM dbo.DaySpaVisit dsv
+WHERE
+	dsv.CustomerVisitStart >= '2020-01-01'
+	AND dsv.CustomerVisitStart < '2021-01-01'
+GROUP BY
+	DATEPART(WEEK, dsv.CustomerVisitStart)
+ORDER BY
+	Week;
+						 
+-- Example 3: (Using a calendar table)
+SELECT
+	-- Determine the week of the calendar year
+	c.CalendarWeekOfYear,
+	-- Determine the earliest date in this group
+	MIN(c.Date) AS FirstDateOfWeek,
+	ISNULL(SUM(dsv.AmenityUseInMinutes), 0) AS AmenityUseInMinutes,
+	ISNULL(MAX(dsv.CustomerID), 0) AS HighestCustomerID,
+	COUNT(dsv.CustomerID) AS NumberOfAttendees
+FROM dbo.Calendar c
+	LEFT OUTER JOIN dbo.DaySpaVisit dsv
+		-- Connect dbo.Calendar with dbo.DaySpaVisit
+		-- To join on CustomerVisitStart, we need to turn 
+        -- it into a DATE type
+		ON c.Date = CAST(dsv.CustomerVisitStart AS DATE)
+WHERE
+	c.CalendarYear = 2020
+GROUP BY
+	-- Arrange into groups
+	c.CalendarWeekOfYear
+ORDER BY
+	c.CalendarWeekOfYear;
+						 
+						 
+						 
+-- Grouping by:
+-- Method 1: (Generate a summary with ROLLUP)
+-- The ROLLUP operator works best when your non-measure attributes are hierarchical.
+-- Otherwise, you may end up weird aggregation levels which don't make intuitive sense.
+SELECT
+	c.CalendarYear,
+	c.CalendarQuarterName,
+	c.CalendarMonth,
+    -- Include the sum of incidents by day over each range
+	SUM(ir.NumberOfIncidents) AS NumberOfIncidents
+FROM dbo.IncidentRollup ir
+	INNER JOIN dbo.Calendar c
+		ON ir.IncidentDate = c.Date
+WHERE
+	ir.IncidentTypeID = 2
+GROUP BY
+	-- GROUP BY needs to include all non-aggregated columns
+	c.CalendarYear,
+	c.CalendarQuarterName,
+	c.CalendarMonth
+-- Fill in your grouping operator
+WITH ROLLUP
+ORDER BY
+	c.CalendarYear,
+	c.CalendarQuarterName,
+	c.CalendarMonth;						 
+						 
+-- Method 2: (View all aggregations with CUBE)
+-- The CUBE operator provides a cross aggregation of all combinations and can be a huge number of rows.
+-- This operator works best with non-hierarchical data where you are interested in independent aggregations as well as the combined aggregations.				 
+SELECT
+	-- Use the ORDER BY clause as a guide for these columns
+    -- Don't forget that comma after the third column if you
+    -- copy from the ORDER BY clause!
+	ir.IncidentTypeID,
+	c.CalendarQuarterName,
+	c.WeekOfMonth,
+	SUM(ir.NumberOfIncidents) AS NumberOfIncidents
+FROM dbo.IncidentRollup ir
+	INNER JOIN dbo.Calendar c
+		ON ir.IncidentDate = c.Date
+WHERE
+	ir.IncidentTypeID IN (3, 4)
+GROUP BY
+	-- GROUP BY should include all non-aggregated columns
+	ir.IncidentTypeID,
+	c.CalendarQuarterName,
+	c.WeekOfMonth
+-- Fill in your grouping operator
+WITH CUBE
+ORDER BY
+	ir.IncidentTypeID,
+	c.CalendarQuarterName,
+	c.WeekOfMonth;
+						 
+-- Method 3: (Generate custom groupings with GROUPING SETS)
+-- The GROUPING SETS operator allows us to define the specific aggregation levels we desire.
+-- To see something similar to a ROLLUP but without quite as much information.
+-- Instead of showing every level of aggregation in the hierarchy
+SELECT
+	c.CalendarYear,
+	c.CalendarQuarterName,
+	c.CalendarMonth,
+	SUM(ir.NumberOfIncidents) AS NumberOfIncidents
+FROM dbo.IncidentRollup ir
+	INNER JOIN dbo.Calendar c
+		ON ir.IncidentDate = c.Date
+WHERE
+	ir.IncidentTypeID = 2
+-- Fill in your grouping operator here
+GROUP BY GROUPING SETS
+(
+  	-- Group in hierarchical order:  calendar year,
+    -- calendar quarter name, calendar month
+	(c.CalendarYear, c.CalendarQuarterName, c.CalendarMonth),
+  	-- Group by calendar year
+	(c.CalendarYear),
+    -- This remains blank; it gives us the grand total
+	()
+)
+ORDER BY
+	c.CalendarYear,
+	c.CalendarQuarterName,
+	c.CalendarMonth;
+						 
+-- Method 4: (Combine multiple aggregations in one query)
+SELECT
+	c.CalendarYear,
+	c.CalendarMonth,
+	c.DayOfWeek,
+	c.IsWeekend,
+	SUM(ir.NumberOfIncidents) AS NumberOfIncidents
+FROM dbo.IncidentRollup ir
+	INNER JOIN dbo.Calendar c
+		ON ir.IncidentDate = c.Date
+GROUP BY GROUPING SETS
+(
+    -- Each non-aggregated column from above should appear once
+  	-- Calendar year and month
+	(c.CalendarYear, c.CalendarMonth),
+  	-- Day of week
+	(c.DayOfWeek),
+  	-- Is weekend or not
+	(c.IsWeekend),
+    -- This remains empty; it gives us the grand total
+	()
+)
+ORDER BY
+	c.CalendarYear,
+	c.CalendarMonth,
+	c.DayOfWeek,
+	c.IsWeekend;
+						 
