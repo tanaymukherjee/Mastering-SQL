e93d1a0979a3082570deca0b73e3de6c146d8da5
Tanay Mukherjee <its_tanay_here@yahoo.com>
2020-06-08 23:19:56 -0400

    Update Date manipulation.sql

--- Date manipulation.sql
+++ Date manipulation.sql
@@ -1,4 +1,4 @@
--- Counting the number of days between dates
+-- Counting the number of days between dates:
 -- Return the difference in OrderDate and ShipDate
 SELECT OrderDate, ShipDate, 
        DATEDIFF(DD, OrderDate, ShipDate) AS Duration
@@ -7,7 +7,7 @@ FROM Shipments
 
 
 
--- Adding days to a date
+-- Adding days to a date:
 -- Return the DeliveryDate as 5 days after the ShipDate
 SELECT OrderDate, 
        DATEADD(DD, 5, ShipDate) AS DeliveryDate 
@@ -15,7 +15,7 @@ FROM Shipments
 
 
 
--- Break out a date into year, month, and day
+-- Break out a date into year, month, and day:
 -- Why we use datetime 2: https://stackoverflow.com/questions/1334143/datetime2-vs-datetime-in-sql-server
 -- datetime2(7) is the default standard
 DECLARE
@@ -28,7 +28,7 @@ SELECT
 
 
 
--- Break a date and time into component parts
+-- Break a date and time into component parts:
 DECLARE
 	@BerlinWallFalls DATETIME2(7) = '1989-11-09 23:49:36.2294852';
 -- Fill in each date part
@@ -46,7 +46,7 @@ SELECT
 
 
 
--- Date math and leap years
+-- Date math and leap years:
 -- Example 1:
 -- To understand date parts and intervals needed to determine how SQL Server works on February 29th of a leap year.
 DECLARE
@@ -79,7 +79,7 @@ SELECT
 
 
 
--- Rounding dates
+-- Rounding dates:
 DECLARE
 	@SomeTime DATETIME2(7) = '2018-06-14 16:29:36.2248991';
 -- Fill in the appropriate functions and date parts
@@ -90,7 +90,7 @@ SELECT
 
 
 
--- Formatting dates with CAST() and CONVERT()
+-- Formatting dates with CAST() and CONVERT():
 DECLARE
 	@CubsWinWorldSeries DATETIME2(3) = '2016-11-03 00:30:29.245',
 	@OlderDateType DATETIME = '2016-11-03 00:30:29.245';
@@ -117,7 +117,7 @@ SELECT
 
 							   
 							   
--- Formatting dates with FORMAT()
+-- Formatting dates with FORMAT():
 -- Example 1:
 -- Use the 'd' format parameter (note that this is case sensitive!)
 DECLARE
@@ -189,7 +189,103 @@ WHERE
 							   
 							   
 							   
-						   
+
+-- Build dates from parts:
+-- The DATEFROMPARTS() function allows us to turn a series of numbers representing date parts into a valid DATE data type.
+-- Example 1:
+SELECT TOP(10)
+	c.CalendarQuarterName,
+	c.MonthName,
+	c.CalendarDayOfYear
+FROM dbo.Calendar c
+WHERE
+	-- Create dates from component parts
+	DATEFROMPARTS(c.CalendarYear, c.CalendarMonth, c.Day) >= '2018-06-01'
+	AND c.DayName = 'Tuesday'
+ORDER BY
+	c.FiscalYear,
+	c.FiscalDayOfYear ASC;
+
+-- We use this clause when days, months, years are gievn seperately in different formats.
+-- Example 2:
+SELECT
+	-- Mark the date and time the lunar module touched down
+    -- Use 24-hour notation for hours, so e.g., 9 PM is 21
+	DATETIME2FROMPARTS(1969, 07, 20, 20, 17, 00, 000, 0) AS TheEagleHasLanded,
+	-- Mark the date and time the lunar module took back off
+    -- Use 24-hour notation for hours, so e.g., 9 PM is 21
+	DATETIMEFROMPARTS(1969, 07, 21, 18, 54, 00, 000) AS MoonDeparture;
+-- Result:
+--  TheEagleHasLanded	   MoonDeparture
+-- 1969-07-20 20:17:00	1969-07-21 18:54:00
+							   
+							   
+							   
+-- Build dates and times with offsets from parts:
+-- Miscellaneous case
+-- The DATETIMEOFFSETFROMPARTS() function builds a DATETIMEOFFSET out of component values.
+-- It has the most input parameters of any date and time builder function.
+-- On January 19th, 2038 at 03:14:08 UTC, we will experience the Year 2038 (or Y2.038K) problem.
+-- This is the moment that 32-bit devices will reset back to the date 1900-01-01.
+-- This runs the risk of breaking every 32-bit device using POSIX time, which is the number of seconds elapsed since January 1, 1970 at midnight UTC.
+
+-- Build a DATETIMEOFFSET which represents the last millisecond before the Y2.038K problem hits. The offset should be UTC.
+-- Build a DATETIMEOFFSET which represents the moment devices hit the Y2.038K issue in UTC time. Then use the AT TIME ZONE operator to convert this to Eastern Standard Time.
+SELECT
+	-- Fill in the millisecond PRIOR TO chaos
+	DATETIMEOFFSETFROMPARTS(2038, 01, 19, 03, 14, 07, 999, 0, 0, 3) AS LastMoment,
+    -- Fill in the date and time when we will experience the Y2.038K problem
+    -- Then convert to the Eastern Standard Time time zone
+	DATETIMEOFFSETFROMPARTS(2038, 01, 19, 03, 14, 08, 0, 0, 0, 3) AT TIME ZONE 'Eastern Standard Time' AS TimeForChaos;
+-- Result:
+-- LastMoment	                        TimeForChaos
+-- 2038-01-19 03:14:07.9990000 +00:00	2038-01-18 22:14:08.0000000 -05:00
+
+							   
+							   
+							   
+-- Cast strings to dates:
+-- The CAST() function allows us to turn strings into date and time data types.
+-- In this example, we will review many of the formats CAST() can handle.							   
+SELECT
+	d.DateText AS String,
+	-- Cast as DATE
+	CAST(d.DateText AS DATE) AS StringAsDate,
+	-- Cast as DATETIME2(7)
+	CAST(d.DateText AS DATETIME2(7)) AS StringAsDateTime2
+FROM dbo.Dates d;
+							   
+							   
+							   
+-- Convert strings to dates
+-- The CONVERT() function behaves similarly to CAST().
+-- When translating strings to dates, the two functions do exactly the same work under the covers.
+-- Although we used all three parameters for CONVERT() during a prior exercise, we will only need two parameters here: the data type and input expression.
+SET LANGUAGE 'GERMAN'
+SELECT
+	d.DateText AS String,
+	-- Convert to DATE
+	CONVERT(DATE, d.DateText) AS StringAsDate,
+	-- Convert to DATETIME2(7)
+	CONVERT(DATETIME2(7), d.DateText) AS StringAsDateTime2
+FROM dbo.Dates d;
+				     
+				     
+				     
+-- Parse strings to dates
+-- Changing our language for data loading is not always feasible.
+-- Instead of using the SET LANGUAGE syntax, we can instead use the PARSE() function to parse a string as a date type using a specific locale.
+-- We will once again use the dbo.Dates table, this time parsing all of the dates as German using the de-de locale.				     
+SELECT
+	d.DateText AS String,
+	-- Parse as DATE using German
+	PARSE(d.DateText AS DATE USING 'de-de') AS StringAsDate,
+	-- Parse as DATETIME2(7) using German
+	PARSE(d.DateText AS DATETIME2(7) USING 'de-de') AS StringAsDateTime2
+FROM dbo.Dates d;
+
+				     
+				     
 -- Write a query to determine how many transactions exist per day.
 -- Use of CONVERT function:
 SELECT
