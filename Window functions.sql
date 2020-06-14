3fa6196f64cc6f261d42f4eea3ffe6d0620278ae
Tanay Mukherjee <its_tanay_here@yahoo.com>
2020-06-13 23:58:13 -0400

    Update Window functions.sql

--- Window functions.sql
+++ Window functions.sql
@@ -757,3 +757,60 @@ $$) AS ct (Country VARCHAR,
            "2012" INTEGER)
 
 Order by Country ASC;						 
+
+						 
+						 					
+-- Month-over-month comparison:
+-- In order to compare months, you need to use one of the following window functions:
+-- LAG(value, offset), which outputs a value from an offset number previous to to the current row in the report.
+-- LEAD(value, offset), which outputs a value from a offset number after the current row in the report.
+-- Your goal is to build a report that shows each country's month-over-month views. A few tips:
+
+-- You will need to bucket dates into months. To do this, you can use the DATE_PART() function.
+-- You can calculate the percent change using the following formula: (value)/(previous_value) - 1.
+-- If no offset value is included in the LAG() or LEAD() functions, it will default to 1.
+-- Since the table stops in the middle of June, the query is set up to only include data to the end of May.
+SELECT
+	-- Pull month and country_id
+	DATE_PART('month', date) AS month,
+	country_id,
+    -- Pull in current month views
+    SUM(views) AS month_views,
+    -- Pull in last month views
+    LAG(SUM(views)) OVER (PARTITION BY country_id ORDER BY DATE_PART('month', date)) AS previous_month_views,
+    -- Calculate the percent change
+    SUM(views) / LAG(SUM(views)) OVER (PARTITION BY country_id ORDER BY DATE_PART('month', date)) - 1 AS perc_change
+FROM web_data
+WHERE date <= '2018-05-31'
+GROUP BY month, country_id;
+										  
+										  
+										  
+-- Week-over-week comparison:
+-- In the previous exercise, you leveraged the set window of a month to calculate month-over-month changes.
+-- But sometimes, you may want to calculate a different time period, such as comparing last 7 days to the previous 7 days.
+-- To calculate a value from the last 7 days, you will need to set up a rolling calculation.
+
+-- In this exercise, you will take the rolling 7 day average of views for each date and compare it to the previous 7 day average for views. This gives a clear week-over-week comparison for every single day.
+-- Syntax for a rolling average is:
+-- AVG(value) OVER (PARTITION BY field ORDER BY field ROWS BETWEEN N PRECEDING AND CURRENT ROW), where N is the number of rows to look back when doing the calculation. Remember that CURRENT ROW counts as a row.										  
+SELECT 
+	-- Pull in date and weekly_avg
+	date,
+    weekly_avg,
+    -- Output the value of weekly_avg from 7 days prior
+    LAG(weekly_avg,7) OVER (ORDER BY date) AS weekly_avg_previous,
+    -- Calculate percent change vs previous period
+    weekly_avg / LAG(weekly_avg,7) OVER (ORDER BY date) - 1 as perc_change
+FROM
+  (SELECT
+      -- Pull in date and daily_views
+      date,
+      SUM(views) AS daily_views,
+      -- Calculate the rolling 7 day average
+      AVG(SUM(views)) OVER (ORDER BY date ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS weekly_avg
+  FROM web_data
+  -- Alias as subquery
+  GROUP BY date) AS subquery
+-- Order by date in descending order
+ORDER BY date DESC;					 
